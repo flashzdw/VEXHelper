637a0508b217f917c17af85e7da7a7723fb9cd28
ZhangDongwei <DongZi8009@163.com>
2026-01-24 13:03:27 +0800

    Initial Commit

---
+++ VEXHelper/VEXHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  VEXHelperApp.swift
+//  VEXHelper
+//
+//  Created by DongZi.8009 on 2026/1/24.
+//
+
+import SwiftUI
+
+@main
+struct VEXHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
